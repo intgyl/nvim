3f29653983f727842f3d47112c347f6e1e0e7aa6
intgyl <intgyl@gmail.com>
2025-05-16 19:03:17 +0800

    enable colorscheme transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 require("tokyonight").setup({
 	style = "storm", -- 可选：storm, moon, night, day
+	transparent = true,
 	on_colors = function(colors)
 		colors.hint = colors.orange
 		colors.error = "#ff0000"
 	end,
 	on_highlights = function(highlights, colors)
 		-- highlights.Function = { fg = colors.blue}
-		highlights.Comment  = { fg = "#8a98c8", italic = true }
+		highlights.Comment  = { fg = "#7f7f7f", italic = true }
 		-- highlights.String   = { fg = "#98c379" }
 		-- highlights.Type     = { fg = colors.green }
 	end,
