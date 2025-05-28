9c7d6f6bebfc76ae896bc8d57f16d9f67b5e3192
intgyl <intgyl@gmail.com>
2025-05-28 08:15:27 +0800

    add default word to live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,8 +45,12 @@ require('telescope').setup({
 
 -- 自定义命令（根据需求调整）
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>fg', function()
+	local word = vim.fn.expand("<cword>")
+	require('telescope.builtin').live_grep({ default_text = word })
+end, { noremap = true, silent = true })
 
 
