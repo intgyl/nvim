404aaa490c5c62222f7a3d94db8590699b0d8392
intgyl <intgyl@gmail.com>
2025-08-08 09:43:56 +0800

    verilog: add verilog support

--- lua/plugins/astyle.lua
+++ lua/plugins/astyle.lua
@@ -1,17 +1,34 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.c", "*.cc", "*.cpp", "*.h", "*.hpp" },
+	pattern = { "*.c", "*.cc", "*.cpp", "*.h", "*.hpp", "*.v" },
 	callback = function()
-		vim.keymap.set('n', '<F4>', function()
-			local filepath = vim.fn.expand('%:p')
-			vim.cmd('write')
-			vim.fn.system({ 'astyle',
-			'--style=linux', '-p','--indent=force-tab=8', '--break-blocks=all',
-			'--indent-switches', '--pad-oper', '--pad-comma', '--pad-header',
-			'--suffix=none', '--align-pointer=name',
-			'--align-reference=name', '--break-one-line-headers',
-			'--attach-return-type', '--attach-return-type-decl', filepath })
-			vim.cmd('edit')
-		end, { buffer = true, desc = 'Format with astyle (F4)' })
+		vim.keymap.set("n", "<F4>", function()
+			local filepath = vim.fn.expand("%:p")
+			vim.cmd("write")
+
+			local ext = vim.fn.expand("%:e") -- 获取文件扩展名（不带 .）
+			if ext == "v" then
+				-- Verilog 使用 verible-verilog-format
+				vim.fn.system({ "verible-verilog-format", "--inplace", "--column_limit=300",
+					"--indentation_spaces=4", "--assignment_statement_alignment=align",
+					"--named_port_alignment=align","--port_declarations_alignment=align",
+					"--module_net_variable_alignment=align",
+					filepath
+				})
+			else
+				-- C/C++ 使用 astyle
+				vim.fn.system({
+					"astyle",
+					"--style=linux", "-p", "--indent=force-tab=8", "--break-blocks=all",
+					"--indent-switches", "--pad-oper", "--pad-comma", "--pad-header",
+					"--suffix=none", "--align-pointer=name", "--align-reference=name",
+					"--break-one-line-headers", "--attach-return-type",
+					"--attach-return-type-decl",
+					filepath
+				})
+			end
+
+			vim.cmd("edit") -- 重新加载格式化后的文件
+		end, { buffer = true, desc = "Format source (F4)" })
 	end,
 })
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 require'nvim-treesitter.configs'.setup {
 	-- 添加不同语言
-	ensure_installed = { "vim", "vimdoc", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+	ensure_installed = { "vim", "vimdoc", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline", "verilog"}, -- one of "all" or a list of languages
 
 	highlight = { enable = true },
 	indent = {
 		enable = true,
-		disable = { "c", "cpp" },
+		disable = { "c", "cpp", "verilog" },
 	},
 }
 
