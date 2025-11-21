3dc2feb91fbf0c0dca936b8977835607571c769f
guanyanliang <guanyanliang@holichip.com>
2025-11-21 14:49:49 +0800

    fix java error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,11 +15,11 @@ require("mason-lspconfig").setup({
 		"pyright",
 		"clangd",
 		"bashls",
-		"java",
+		"jdtls",
 	},
 })
 
 vim.lsp.enable('pyls')
 vim.lsp.enable('clangd')
 vim.lsp.enable('lua_ls')
-vim.lsp.enable('java')
+vim.lsp.enable('jdtls')
