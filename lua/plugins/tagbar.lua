d899300ac155785a2f324f9752f81b656c963443
guanyanliang <guanyanliang@holichip.com>
2025-11-21 18:12:43 +0800

    let tagbar sliently

--- lua/plugins/tagbar.lua
+++ lua/plugins/tagbar.lua
@@ -1,12 +1,13 @@
 -- tagbar
 vim.cmd([[
-  autocmd FileType * if &filetype !=# 'go' | let g:tagbar_width = 35 | let g:tagbar_left = 1 | endif
+	autocmd FileType * if &filetype !=# 'go' | let g:tagbar_width = 35 | let g:tagbar_left = 1 | endif
 
-  if !exists('g:tagbar_keymap_set')
-    let g:tagbar_keymap_set = 1
-    nnoremap <F2> :TagbarToggle<CR>
-  endif
+	if !exists('g:tagbar_keymap_set')
+		let g:tagbar_keymap_set = 1
+		nnoremap <F2> :TagbarToggle<CR>
+	endif
 
-  autocmd VimEnter * if &filetype !=# 'go' | call tagbar#autoopen(1) | endif
+	autocmd VimEnter * if &filetype !=# 'go' | call tagbar#autoopen(1) | endif
 ]])
 
+vim.cmd([[let g:tagbar_silent = 1]])
