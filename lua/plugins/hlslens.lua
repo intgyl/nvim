c14e171cd04e5507db377b7b02929c8822646e88
guanyanliang <guanyanliang@holichip.com>
2025-11-25 15:06:55 +0800

    add keymap for hlslens

--- lua/plugins/hlslens.lua
+++ lua/plugins/hlslens.lua
@@ -23,6 +23,7 @@ vim.api.nvim_set_keymap('n', 'n',
 vim.api.nvim_set_keymap('n', 'N',
     [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
 vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', ';', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
 vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
 
