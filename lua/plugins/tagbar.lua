ea806aca0db01b277835dd47109e0e9f08a191bc
guanyanliang <guanyanliang@holichip.com>
2025-11-28 18:45:28 +0800

    tagbar: add windows support

--- lua/plugins/tagbar.lua
+++ lua/plugins/tagbar.lua
@@ -1,4 +1,12 @@
 -- tagbar
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+	vim.opt.shell = 'cmd.exe'
+	vim.opt.shellcmdflag = '/c'
+	vim.opt.shellquote = '"'
+	vim.opt.shellxquote = ''
+	vim.g.tagbar_ctags_bin = 'C:\\Windows\\System32\\ctags.exe'
+end
+
 vim.cmd([[
 	autocmd FileType * if &filetype !=# 'go' | let g:tagbar_width = 35 | let g:tagbar_left = 1 | endif
 
