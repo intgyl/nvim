2693e8683b85343c4387bb7d9bddcd8e270f67b3
guanyanliang <guanyanliang@holichip.com>
2025-12-18 16:04:14 +0800

    use globale statusline

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -13,6 +13,7 @@ opt.termguicolors = true    -- 启用真彩色
 opt.cursorline = true       -- 光标行
 opt.wrap = true             -- 自动换行
 opt.swapfile = false
+opt.laststatus = 3
 
 vim.g.c_syntax_for_h = 1
 
