bf1154b75aa439a1b6d1f9dc5497f35510cf9165
guanyanliang <guanyanliang@holichip.com>
2025-12-19 14:45:27 +0800

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,84 @@
 require('lualine').setup({
-	sections ={
-		lualine_c = {
-			{'filename', path = 3},
-		},
-
+	sections = {
 		lualine_a = {
 			{
-				'filesize',
+				"mode",
+				color = { fg = "#1f2335", bg = "#7aa2f7" },
+				separator = { right = "" },
 			},
 
 			{
-				'searchcount',
-				maxcount = 999,
-				timeout = 500,
+				'filesize',
+				color = { fg = "#1f2335", bg = "#7dcfff" },
+				separator = { right = "" },
+			},
+
+		},
+
+		lualine_b = {
+			{
+				"diff",
+				symbols = {
+					added    = " ",
+					modified = " ",
+					removed  = " ",
+				},
+				separator = { right = "" },
+			},
+
+			{
+				"branch",
+				icon = "",
+				color = { fg = "#1f2335", bg = "#9ece6a" },
+				separator = { right = "" },
+			},
+
+			{
+				"searchcount",
+				color = { fg = "#1f2335", bg = "#e0af68" },
+				separator = { right = "" },
+			},
+
+		},
+
+		lualine_c = {
+			{
+				'filename', path = 3,
+				color = { fg = "#c0caf5", bg = "#3b4261" },
+			},
+
+		},
+
+		lualine_x = {
+			-- gitblame,
+			{
+				"encoding",
+				color = { fg = "#c0caf5", bg = "#414868" },
+				separator = { left = "" },
+			},
+			{
+				"fileformat",
+				color = { fg = "#c0caf5", bg = "#414868" },
+			},
+			{
+				"filetype",
+				color = { fg = "#c0caf5", bg = "#414868" },
 			},
 		},
-	}
+
+		lualine_y = {
+			{
+				"progress",
+				color = { fg = "#1f2335", bg = "#e0af68" },
+			},
+		},
+
+		lualine_z = {
+			{
+				"location",
+				color = { fg = "#c0caf5", bg = "#3b4261" },
+			},
+		},
+	},
+
 })
