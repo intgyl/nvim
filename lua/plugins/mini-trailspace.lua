7f3493999d681b2bfc682ef109dcd58b501de1f4
intgyl <intgyl@gmail.com>
2026-05-07 16:01:16 +0800

    add MiniTrailspace.trim shortcut

--- lua/plugins/mini-trailspace.lua
+++ lua/plugins/mini-trailspace.lua
@@ -1,2 +1,6 @@
 require("mini.trailspace").setup({
 })
+
+vim.keymap.set("n", "<leader>d", function()
+	MiniTrailspace.trim()
+end, { desc = "Trim trailing whitespace", silent = true, noremap = true })
