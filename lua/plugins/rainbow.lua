e0bb3f1f78ed7cb234bf67a87bc40c6ebe2e79f8
intgyl <intgyl@gmail.com>
2026-06-01 17:15:37 +0800

    rainbow: fix rainbow colors

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,23 +1,33 @@
-config = function()
-	local rainbow_delimiters = require("rainbow-delimiters")
-
-	vim.g.rainbow_delimiters = {
-		strategy = {
-			[''] = rainbow_delimiters.strategy['global'],
-			vim = rainbow_delimiters.strategy['local'],
-		},
-		query = {
-			[''] = 'rainbow-delimiters',
-			lua = 'rainbow-blocks',
-		},
-		highlight = {
-			'RainbowDelimiterRed',
-			'RainbowDelimiterYellow',
-			'RainbowDelimiterBlue',
-			'RainbowDelimiterOrange',
-			'RainbowDelimiterGreen',
-			'RainbowDelimiterViolet',
-			'RainbowDelimiterCyan',
-		},
+local function wrap_strategy(orig)
+	return {
+		on_attach = function(bufnr, settings)
+			settings.parser:parse()
+			return orig.on_attach(bufnr, settings)
+		end,
+		on_detach = orig.on_detach,
+		on_reset = function(bufnr, settings)
+			settings.parser:parse()
+			return orig.on_reset(bufnr, settings)
+		end,
 	}
 end
+
+require('rainbow-delimiters.setup').setup {
+	strategy = {
+		[''] = wrap_strategy(require('rainbow-delimiters.strategy.global')),
+		vim = wrap_strategy(require('rainbow-delimiters.strategy.local')),
+	},
+	query = {
+		[''] = 'rainbow-delimiters',
+		latex = 'rainbow-blocks',
+	},
+	highlight = {
+		'RainbowDelimiterRed',
+		'RainbowDelimiterYellow',
+		'RainbowDelimiterBlue',
+		'RainbowDelimiterOrange',
+		'RainbowDelimiterGreen',
+		'RainbowDelimiterViolet',
+		'RainbowDelimiterCyan',
+	},
+}
