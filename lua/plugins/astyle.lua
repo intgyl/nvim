34310cd7831544e196810bb4f42b8e0d8f28e698
intgyl <intgyl@gmail.com>
2026-08-28 11:16:07 +0800

    ast: update astyle config
    update astyle config

--- lua/plugins/astyle.lua
+++ lua/plugins/astyle.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 				vim.fn.system({
 					"astyle",
 					"--style=linux", "-p", "--indent=force-tab=8", "--break-blocks=all",
-					"indent-switches", "--pad-oper", "--pad-comma", "--pad-header",
+					"--pad-oper", "--pad-comma", "--pad-header",
 					"--suffix=none", "--align-pointer=name", "--align-reference=name",
 					"--break-one-line-headers", "--attach-return-type",
 					"--attach-return-type-decl",
